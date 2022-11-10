5c2e469aefb1377d610aebce253f52ff4f7b4eae
TahjD <tahjjobs@gmail.com>
2022-11-10 11:11:28 -0800

    WORM GAME WORKING CSS NOT DONE

--- app.js
+++ app.js
@@ -1,114 +1,123 @@
-//game Constants and Variables
+// | VARIABLES |
 let inputDir = { x: 0, y: 0 };
-// const foodNoise = new Audio('music/food.mp3');
-// const gameOverNoise = new Audio('music/gameover.mp3')
-let speed = 7;
+let speed = 10;
+let lastDrawTime = 0;
+let snakeArray = [{ x: 5, y: 15 }];
+let food = { x: 14, y: 7 };
 let score = 0;
-let lastPaintTime = 0;
-let nextDirection = { x: 0, y: 1 };
-let worm = [
-  { x: 10, y: 10 },
-  { x: 11, y: 10 },
-  { x: 12, y: 10 },
-  { x: 13, y: 10 },
-  { x: 13, y: 11 },
-  { x: 13, y: 12 },
-];
-food = { x: 7, y: 8 };
 
-//worm
-function draw() {
-  let board = document.getElementById("board");
+// | MAIN LOOP |
+
+function main(initialTime) {
+  window.requestAnimationFrame(main);
+  //console.log(initialTime);
+  if ((initialTime - lastDrawTime) / 1000 < 1 / speed) {
+    return;
+  }
+  lastDrawTime = initialTime;
+  gameEngine();
+}
+
+function isCollide(snake) {
+  // return false;
+
+  // if you bump into your body
+  for (let i = 1; i < snakeArray.length; i++) {
+    if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
+      return true;
+    }
+  }
+  if (
+    snake[0].x >= 18 ||
+    snake[0].x <= 0 ||
+    snake[0].y >= 18 ||
+    snake[0].y <= 0
+  ) {
+    return true;
+  }
+}
+//{gameEngine}
+function gameEngine() {
+  //update the snake + food arrays
+  if (isCollide(snakeArray)) {
+    inputDir = { x: 0, y: 0 };
+    alert("GAMEOVER!.....TRY AGIAN?");
+    snakeArray = [{ x: 13, y: 15 }];
+    score = 0;
+  }
+  //if snake eats ++ to scare
+  if (snakeArray[0].y === food.y && snakeArray[0].x === food.x) {
+    snakeArray.unshift({
+      x: snakeArray[0].x + inputDir.x,
+      y: snakeArray[0].y + inputDir.y,
+    });
+    let a = 2;
+    let b = 16;
+    food = {
+      x: Math.round(a + (b - a) * Math.random()),
+      y: Math.round(a + (b - a) * Math.random()),
+    };
+    score += 1;
+    highScore.innerHTML = +score;
+  }
+  //moving the snake
+  for (let i = snakeArray.length - 2; i >= 0; i--) {
+    snakeArray[i + 1] = { ...snakeArray[i] };
+  }
+  snakeArray[0].x += inputDir.x;
+  snakeArray[0].y += inputDir.y;
+
+  //display the snake using dom to create divs in js
   board.innerHTML = "";
-  worm.forEach((seg) => {
-    let div = document.createElement("div");
+  snakeArray.forEach((e, index) => {
+    snakeElement = document.createElement("div");
+    snakeElement.style.gridRowStart = e.y;
+    snakeElement.style.gridColumnStart = e.x;
+    snakeElement.classList.add("snake");
+    board.appendChild(snakeElement);
 
-    div.style.gridRowStart = seg.y;
-    div.style.gridColumnStart = seg.x;
-    div.classList.add("worm");
-
-    board.appendChild(div);
+    if (index === 0) {
+      snakeElement.classList.add("head");
+    } else {
+      snakeElement.classList.add("snake");
+    }
+    //adding snake to board
   });
+
+  //display food using dom to create divs in js
+  foodElement = document.createElement("div");
+  foodElement.style.gridRowStart = food.y;
+  foodElement.style.gridColumnStart = food.x;
+  foodElement.classList.add("food");
+  board.appendChild(foodElement);
+  //adding food to board
 }
 
-// gameloop
+window.requestAnimationFrame(main);
 
-setInterval(function gameLoop() {
-  moveWorm();
-  // collision
-  // eats food
-  // hits walll
-  draw();
-
-  //console.log('working')
-}, 500);
-
-function moveWorm() {
-  let newHead = {};
-  let wormHead = worm[worm.length - 1];
-  newHead.x = nextDirection.x + wormHead.x;
-  newHead.y = nextDirection.y + wormHead.y;
-  worm.shift();
-  worm.push(newHead);
-
-  console.log(worm);
-  // worm.push({x:13, y:13}
-}
-
-// function gameLoop (curTime){
-//     update()
-
-//
-//     if ((gameLoop - lastPaintTime)/1000 / <1/speed){
-//         return;
-//     }
-//     lastPaintTime = curTime
-//     update();
-// }
-
-// function sCollide(sWorm) {
-//     //if u hit yourself
-//     for (let i = 1; i < worm.length; i++) {
-//         if (sWorm[i].x === sWorm[0].y){
-//             return true;
-//         }
-//         // if you bump into the wall
-//         if (sWorm[0].x <=20 || sWorm[0].x <0 ||sWorm[0].y >= 20 || sWorm[0].y <=0){
-//             return true
-//         }
-//         return false
-//     }
-
-// }
-
-// function update(){
-//     // updating the worm array and food
-//     if (sCollide(worm)){
-//         gameOverNoise.play();
-//         inputDir = {x:13 y:15};
-//         alert("gameover press anykey to play")
-//         score = 0;
-
-//     }
-// }
-// +1 score when food is eaten
-
-// if(worm[0].y === food.y && worm[0].x === food.x){
-//     foodNoise.play()
-//     score += 1;
-
-// }
-
-// score.innerHTML
-
-// window.requestAnimationFrame(gameLoop);
-
-// function food(){
-
-// }
-
-// 1. draw snake (hardcoded)
-// 2. game loop where snake can move
-// 3. collision
-// 4. food
-// 5. game speeding up
+//game inputs
+window.addEventListener("keydown", (e) => {
+  inputDir = { x: 0, y: 1 }; //game begin
+  switch (e.key) {
+    case "ArrowUp":
+      // console.log("ArrowUp");
+      inputDir.x = 0;
+      inputDir.y = -1;
+      break;
+    case "ArrowDown":
+      // console.log("ArrowDown");
+      inputDir.x = 0;
+      inputDir.y = 1;
+      break;
+    case "ArrowLeft":
+      // console.log("ArrowLeft");
+      inputDir.x = -1;
+      inputDir.y = 0;
+      break;
+    case "ArrowRight":
+      // console.log("ArrowRight");
+      inputDir.x = 1;
+      inputDir.y = 0;
+      break;
+  }
+});
